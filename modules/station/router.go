0ad11259b39124a1116d1481084d2fb2448a9c91
Fakhrizal Mustaqim <135151144+fakhrizalmus@users.noreply.github.com>
2024-12-03 18:21:43 +0700

    add getByIdStation

--- modules/station/router.go
+++ modules/station/router.go
@@ -13,6 +13,9 @@ func Initiate(router *gin.RouterGroup) {
 	station.GET("", func(c *gin.Context) {
 		GetAllStation(c, stationService)
 	})
+	station.GET("/:id", func(c *gin.Context) {
+		CheckSchedulesByStation(c, stationService)
+	})
 }
 
 func GetAllStation(c *gin.Context, service Service) {
@@ -33,6 +36,32 @@ func GetAllStation(c *gin.Context, service Service) {
 		response.APIResponse{
 			Success: true,
 			Message: "Success get all stations",
+			Data:    datas,
+		},
+	)
+}
+
+func CheckSchedulesByStation(c *gin.Context, service Service) {
+	id := c.Param("id")
+
+	datas, err := service.CheckScheduleByStation(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest,
+			response.APIResponse{
+				Success: false,
+				Message: err.Error(),
+				Data:    nil,
+			},
+		)
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		response.APIResponse{
+			Success: true,
+			Message: "Success get schedule by station",
 			Data:    datas,
 		},
 	)
